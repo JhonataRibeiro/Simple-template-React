4d787e2cf9f8aca9f2c8a1d94395b2aa91eae822
Jhonata Ribeiro <jhonata.ribeiro.ti@gmail.com>
2016-07-29 17:01:25 -0300

    create footer and import on main container

---
+++ src/app/Components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Footer extends Component {
+
+  render() {
+    const style = require('./Footer.scss');
+    return (
+      <div>
+          This is a footer
+    </div>
+    );
+  }
+}
+
+export default Footer;
--- src/app/Container/App/App.js
+++ src/app/Container/App/App.js
@@ -1,1 +1,3 @@
 /* This is main file */
+import React, { Component, PropTypes } from 'react';
+import { Footer, NavBar } from 'components';
