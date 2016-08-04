00083971e85f3db1d2e12eb94739b11531bc502d
Jhonata Ribeiro <jhonata.ribeiro.ti@gmail.com>
2016-08-04 14:06:13 -0300

    turn inline component

--- src/app/client.js
+++ src/app/client.js
@@ -4,9 +4,6 @@ import { IndexRoute, Route, Router, browserHistory }  from 'react-router';
 import Routes from './routes';
 import { App } from './container';
 
-
-
-	const component = (<Router history={browserHistory} routes={Routes()}>
-	</Router>);
+const component = (<Router history={browserHistory} routes={Routes()}/>);
 
 ReactDOM.render(component, document.getElementById('root'));
