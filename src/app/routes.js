3f00685e44876b8a446d942c2b9ffef5f814c7bf
Jhonata Ribeiro <jhonata.ribeiro.ti@gmail.com>
2016-08-04 13:33:35 -0300

    fix route

--- src/app/routes.js
+++ src/app/routes.js
@@ -1,11 +1,12 @@
 import React from "react";
 import React-dom from "react-dom";
 import { IndexRoute, Route } from 'react-router';
-import App from "./app"
+import App from "./container/App"
 
 export default () => {
 	return(
 		<Route path="/" component={ App }>
+			<IndexRoute component={ App }/>
 		</Route>
 	)
 }
