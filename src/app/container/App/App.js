6f70d32326cf1f672565fca15225442d260763ae
Jhonata Ribeiro <jhonata.ribeiro.ti@gmail.com>
2016-08-24 16:29:41 -0300

    remove trash

--- src/app/container/App/App.js
+++ src/app/container/App/App.js
@@ -2,29 +2,26 @@ import React, { Component, PropTypes } from 'react';
 import { Footer } from '../../components';
 
 class App extends Component {
-  
-  static propTypes = {
-  	children: PropTypes.object.isRequired
-  };
 
-  render() {
-  	console.log('App container rendering');
-    const styles = require('./App.scss');
-    console.log(styles);
-    return (
-      <div id='outer-container'>
-          <main id='page-wrap'>
-      			   <div>
-                { this.props.children }
-               </div>
-               <div className={styles.app}>
-                  <div className={styles.brand}>Jhonata Ribeiro</div>
-               </div>
-               <Footer/>
-      		</main>
-      </div>
-    );
-  }
+    static propTypes = {
+        children: PropTypes.object.isRequired
+    };
+
+    render() {
+        console.log('App container rendering');
+        const styles = require('./App.scss');
+        console.log(styles);
+        return (
+            <div id='outer-container'>
+                <main id='page-wrap'>
+                    <div>
+                        { this.props.children }
+                    </div>
+                    <Footer/>
+                </main>
+            </div>
+        );
+    }
 }
 
 export default App;
