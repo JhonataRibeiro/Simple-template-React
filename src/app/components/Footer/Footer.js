f79bcbe9d2a14b1491fb954632f7a186da7bd601
Jhonata Ribeiro <jhonata.ribeiro.ti@gmail.com>
2016-09-02 16:00:05 -0300

    remove style from footer (of title) and put on component title

--- src/app/components/Footer/Footer.js
+++ src/app/components/Footer/Footer.js
@@ -4,12 +4,11 @@ class Footer extends Component {
   render() {
   	const styles = require('./Footer.scss');
     return (
-      <div>
-          <div className='footer'>
+      <div className='footer'>
+            {/*TODO:O footer não deve ser fixo, ele deve se ajustar ao conteudo com alguma margem de padding*/}
           	{/*TODO:Deve ser um titulo com props: fonte-size, padding acima e abaixo, padding lados*/}
             {/*TODO:Deve ter uma opção de centralizar dentro da div*/}
  		         <Title/>
-          </div>
     </div>
     );
   }
--- src/app/components/Title/Title.js
+++ src/app/components/Title/Title.js
@@ -4,15 +4,12 @@ class Title extends Component {
 	render(){
 		const styles = require('./Title.scss');
 		return(
-			<div>
 				<div className='title'>
-					
 					<h2>Tornomec</h2>
 				</div>
-			</div>
 		);
 
 	}
 }
 
-export default Title;
+export default Title
