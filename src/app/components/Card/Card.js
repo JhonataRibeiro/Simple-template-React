9b970e588abd69a342cee721af5ea237fa0ea145
Jhonata Ribeiro <jhonata.ribeiro.ti@gmail.com>
2016-09-06 16:35:52 -0300

    structure for cards

---
+++ src/app/components/Card/Card.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+class Card extends Component {
+  render() {
+  	const styles = require('./Card.scss');
+    return (
+      <div className='container'>
+    </div>
+    );
+  }
+}
+export default Card;
