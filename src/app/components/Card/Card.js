25c60949d112548f448684e93001cb0f4540f408
Jhonata Ribeiro <jhonata.ribeiro.ti@gmail.com>
2016-09-09 13:55:27 -0300

    create structure of card

--- src/app/components/Card/Card.js
+++ src/app/components/Card/Card.js
@@ -2,14 +2,28 @@ import React, {Component} from 'react'
 class Card extends Component {
   render() {
   	const styles = require('./Card.scss');
+    const cardImage = {
+        background:'red',
+        width:'100%',
+        height:'40px',
+    }
     return (
       //TODO: O container pode ser tornar um component?
       <div className='container-card'>
-          <div className="card">
-            Box Jhonata
-          </div>
-          <div className="card">
-            Box Ribeiro
+          <div className="cards">
+              <div className="card">
+                {/*Image of card*/}
+                <div style={ cardImage }></div>
+                <span>Card One</span>
+              </div>
+              <div className="card">
+                <div style={ cardImage }></div>
+                <span>Card Two</span>
+              </div>
+              <div className="card">
+                <div style={ cardImage }></div>
+                <span>Card Three</span>
+              </div>
           </div>
       </div>
     );
